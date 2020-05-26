c52b0729d7483593bc6ebb6d52e3a7baec008034
taotaoing <1906371514@qq.com>
2020-05-26 21:42:16 +0800

    申涛涛 init

---
+++ src/main/java/com/simple/generator/SimpleGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.simple.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGeneratorApplication.class, args);
+	}
+
+}
